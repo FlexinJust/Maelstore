3872b0ef92bba50708e28610bb92a15bc78dff47
FlexinJust <74312463+FlexinJust@users.noreply.github.com>
2020-11-20 18:28:37 +0200

    new site

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,19 @@
-let menu = document.querySelector('.menu');
-let menuIcon = menu.querySelector('.menu__icon');
-let menuBody = menu.querySelector('.menu__body');
+// let menu = document.querySelector('.header__menu-burger');
+// let menuIcon = menu.querySelector('.burger__icon');
+// let menuBody = menu.querySelector('.menu__body');
 
-menuIcon.onclick = function() {
-    menuBody.classList.toggle('active');
-};
+// menuIcon.onclick = function() {
+//     menuBody.classList.toggle('active');
+// };
 
-document.addEventListener('click', (e) => {
-    if(e.pageX > parseInt(window.getComputedStyle(menuBody).width)) {
-        menuBody.classList.remove('active');
-    }
-});
+// document.addEventListener('click', (e) => {
+//     if(e.pageX > parseInt(window.getComputedStyle(menuBody).width)) {
+//         menuBody.classList.remove('active');
+//     }
+// });
+
+function openMenu() {
+	document.getElementById("sidebar").classList.toggle('active');
+	document.getElementById("burger__icon").classList.toggle('active');
+	document.getElementById("burger__line").classList.toggle('active');
+}
