271b952e71d356eac71a7d6139f6b31a5ea448ee
FlexinJust <74312463+FlexinJust@users.noreply.github.com>
2020-11-21 20:58:46 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,33 @@
-// let menu = document.querySelector('.header__menu-burger');
-// let menuIcon = menu.querySelector('.burger__icon');
+// let menu = document.querySelector('.menu');
+// let menuIcon = menu.querySelector('.menu__icon');
 // let menuBody = menu.querySelector('.menu__body');
 
 // menuIcon.onclick = function() {
 //     menuBody.classList.toggle('active');
 // };
 
-// document.addEventListener('click', (e) => {
-//     if(e.pageX > parseInt(window.getComputedStyle(menuBody).width)) {
-//         menuBody.classList.remove('active');
-//     }
-// });
+document.addEventListener('click', (e) => {
+    if(e.pageX > parseInt(window.getComputedStyle(menuBody).width)) {
+        menuBody.classList.remove('active');
+    }
+});
 
-function openMenu() {
+// function openMenu() {
+// 	document.getElementById("sidebar").classList.toggle('active');
+// 	document.getElementById("burger__icon").classList.toggle('active');
+// 	document.getElementById("burger__line").classList.toggle('active');
+// };
+
+// const menuSidebar = document.getElementById("burger__icon");
+
+// menuSidebar.onclick = function(){
+// 	menuSidebar.classList.toggle('active');
+// };
+
+
+burger__icon.onclick = function () {
 	document.getElementById("sidebar").classList.toggle('active');
-	document.getElementById("burger__icon").classList.toggle('active');
-	document.getElementById("burger__line").classList.toggle('active');
-}
+	this.classList.toggle('active');
+	// document.querySelector('.sidebar-overlay').classList.toggle('active');
+
+};
